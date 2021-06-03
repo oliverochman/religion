071152ceb84aed04fe6b3b59198a36b9ffc8a61e
oliverochman <oliver.ochman@hotmail.com>
2021-06-03 20:47:32 +0200

    remove sticky sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,49 +1,41 @@
-import React, { useState, createRef } from 'react'
+import React, { useState } from 'react'
 import {
   Link
 } from "react-router-dom";
-import { Menu, Divider, Ref, Sticky, Rail } from 'semantic-ui-react'
+import { Menu, Divider } from 'semantic-ui-react'
 import AboutMe from './AboutMe'
 
 
 const Sidebar = () => {
-  const contextRef = createRef()
-  const [activeItem, setActiveItem] = useState('posts')
 
   return (
     <>
-      <Ref innerRef={contextRef}>
-        <Rail>
-          <Sticky active={true} context={contextRef}>
-            <Divider section>
-              <Menu fluid vertical tabular>
-                <Menu.Item
-                  active={activeItem === 'posts'}
-                  onClick={() => setActiveItem('posts')}
-                  as={Link}
-                  to="/"
-                >
-                  Posts
+      <Divider section>
+        <Menu fluid vertical tabular>
+          <Menu.Item
+            active={activeItem === 'posts'}
+            onClick={() => setActiveItem('posts')}
+            as={Link}
+            to="/"
+          >
+            Posts
         </Menu.Item>
 
-                <Menu.Item
-                  active={activeItem === 'online-resources'}
-                  onClick={() => setActiveItem('online-resources')}
-                  as={Link}
-                  to="/online-resources"
-                >
-                  Online resources
+          <Menu.Item
+            active={activeItem === 'online-resources'}
+            onClick={() => setActiveItem('online-resources')}
+            as={Link}
+            to="/online-resources"
+          >
+            Online resources
         </Menu.Item>
 
 
-                <Divider section />
-              </Menu>
-              <AboutMe />
-            </Divider>
+          <Divider section />
+        </Menu>
+        <AboutMe />
+      </Divider>
 
-          </Sticky>
-        </Rail>
-      </Ref>
     </>
   )
 }
